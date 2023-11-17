a58e3584ca0d3f9e76d5a1e131a9ff09c3070cd5
Johann <jekneto@hotmail.com>
2023-11-17 10:08:25 +0100

    arrays test

--- bmi-calculator/array.js
+++ bmi-calculator/array.js
@@ -1,8 +1,29 @@
-const array = ["hello", "world", 42, true]
+// const array = ["hello", "world", 42, true]
+console.log("What does process.argv contain?", process.argv);
 
 
+// const firstElement = process.argv[0]
+// const secondElement = process.argv[1]
+// const thirdElement = process.argv[2]
+// const fourthElement = process.argv[3]
 
-const firstElement = array[0]
+// console.log("FIRST ELEMENTE =>", firstElement)
+// console.log("Second ELEMENTE =>", secondElement)
+// console.log("Third  ELEMENTE =>", thirdElement)
+// console.log("Fourth ELEMENTE =>", fourthElement)
+
+
+const myArray = ["hello", "world", 42, true]
+
+const firstElement = myArray[0]
+const secondElement = myArray[1]
+const thirdElement = myArray[2]
+const fourthElement = myArray[3]
 
 
 console.log("FIRST ELEMENTE =>", firstElement)
+console.log("Second ELEMENTE =>", secondElement)
+console.log("Third  ELEMENTE =>", thirdElement)
+console.log("Fourth ELEMENTE =>", fourthElement)
+
+console.log(firstElement +""+ secondElement)
