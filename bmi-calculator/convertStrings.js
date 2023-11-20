21985dab9dcb12a4eddefd0d10368f2fad87efb3
Johann <jekneto@hotmail.com>
2023-11-20 14:12:35 +0100

    working BMI

---
+++ bmi-calculator/convertStrings.js
@@ -0,0 +1,4 @@
+console.log ("PROCESS ARGV", process.argv)
+
+const numberConvertedFromString = Number("1.79")
+console.log(numberConvertedFromString)
--- bmi-calculator/index.js
+++ bmi-calculator/index.js
@@ -1,25 +1,25 @@
 // bmi-calculator formula --> BMI= weight (kg) / (height (m) x height (m))
 console.log("What does process.argv contain?", process.argv);
 
-let weightInKg = 82
-let heightInKg = 1.79
+const weightInKg = process.argv[2]
+const heightInM = process.argv[3]
 
-// const BMI = Math.round(weightInKg / (heightInKg * heightInKg))
+const BMI = Math.round(weightInKg / (heightInM * heightInM))
 
 console.log(`**************
 BMI CALCULATOR
 **************
 
-height: ${heightInKg }
+height: ${heightInM }
 weight: ${weightInKg}
 
 ****************
 FACING THE FACTS
 ****************
 
-Your BMI is ${Math.round(weightInKg / (heightInKg * heightInKg))
-}
 
+Your BMI is ${BMI
+}
 A BMI under 18.5 is considered underweight
 A BMI above 25 is considered overweight`)
 
